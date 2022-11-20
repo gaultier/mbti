b57d85c35b384950f54c39b03b2ab8493a7f93b8
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-11-20 20:12:09 +0100

    get show detail

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -14,16 +15,27 @@ const (
 	ApiUrl = "https://api.themoviedb.org/3"
 )
 
-type Series struct {
+type ShowSummary struct {
 	Id          uint64
 	Name        string
 	Overview    string
 	VoteAverage float32 `json:"vote_average"`
 }
 
-type Response struct {
+type SearchSeriesResponse struct {
 	Page    uint64
-	Results []Series
+	Results []ShowSummary
+}
+
+type Season struct {
+	Id           uint64
+	Name         string
+	Overview     string
+	SeasonNumber uint64 `json:"season_number"`
+}
+
+type ShowFull struct {
+	Seasons []Season
 }
 
 func main() {
@@ -31,24 +43,54 @@ func main() {
 	client := http.Client{Timeout: 10 * time.Second}
 
 	apiKey := os.Getenv("API_KEY")
-	page := 1
-	query := "Friends" // FIXME
-	res, err := client.Get(fmt.Sprintf("%s/search/tv?page=%d&api_key=%s&query=%s", ApiUrl, page, apiKey, url.QueryEscape(query)))
-	if err != nil {
-		panic(err) // TODO: improve
+	if apiKey == "" {
+		log.Fatalf("No API_KEY environment variable set")
 	}
 
-	if res.StatusCode != 200 {
-		log.Panicf("Non 200 response: %s %v", res.Status, res.Body)
+	// Search shows.
+	var pickedShow *ShowSummary
+	{
+		page := 1
+		query := "Friends" // FIXME
+		res, err := client.Get(fmt.Sprintf("%s/search/tv?page=%d&api_key=%s&query=%s", ApiUrl, page, apiKey, url.QueryEscape(query)))
+		if err != nil {
+			panic(err) // FIXME
+		}
+
+		if res.StatusCode != 200 {
+			log.Panicf("Non 200 response: %s %v", res.Status, res.Body)
+		}
+
+		response := SearchSeriesResponse{Results: make([]ShowSummary, 0, 500)}
+		j := json.NewDecoder(res.Body)
+		err = j.Decode(&response)
+		if err != nil {
+			panic(err) // FIXME
+		}
+
+		log.Println(response.Results[0:5])
+		pickedShow = &response.Results[0] // FIXME
 	}
 
-	response := Response{Results: make([]Series, 0, 500)}
-	j := json.NewDecoder(res.Body)
-	err = j.Decode(&response)
-	if err != nil {
-		panic(err) // TODO: improve
+	{
+		url := fmt.Sprintf("%s/tv/%d?api_key=%s", ApiUrl, pickedShow.Id, apiKey)
+		res, err := client.Get(url)
+		if err != nil {
+			panic(err) // FIXME
+		}
+
+		if res.StatusCode != 200 {
+			body, _ := io.ReadAll(res.Body)
+			log.Panicf("Non 200 response: url=%s status=%s body=%s", url, res.Status, body)
+		}
+
+		show := ShowFull{}
+		j := json.NewDecoder(res.Body)
+		err = j.Decode(&show)
+		if err != nil {
+			panic(err) // FIXME
+		}
+
+		log.Println(show)
 	}
-
-	log.Println(response.Results[0:5])
-
 }
